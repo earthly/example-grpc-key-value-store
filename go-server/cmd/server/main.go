628e8152d33a6edf3bd8470c582c2765abae4c8f
Alex Couture-Beil <alex@earthly.dev>
2021-09-13 10:55:48 -0700

    fix go server and add integration test
    updates go server to use new location of generated proto buf code,
    and adds an integration test under ./test+all
    
    the test was intentially left out from the root +all target, as it
    requires a -P (and I would rather not force users to initially run
    the code with the -P mode as it might be their first time using
    earthly).
    
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- go-server/cmd/server/main.go
+++ go-server/cmd/server/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/earthly/example-grpc-key-value-store/go-server/kvapi"
+	"github.com/earthly/example-grpc-key-value-store/go-server/kvapi"
 
 	"google.golang.org/grpc"
 )
@@ -19,24 +19,24 @@ var errKeyNotFound = fmt.Errorf("key not found")
 
 // server is used to implement kvapi.KeyValueServer
 type server struct {
-	pb.UnimplementedKeyValueServer
+	kvapi.UnimplementedKeyValueServer
 	data map[string]string
 }
 
 // Set stores a given value under a given key
-func (s *server) Set(ctx context.Context, in *pb.SetRequest) (*pb.SetReply, error) {
+func (s *server) Set(ctx context.Context, in *kvapi.SetRequest) (*kvapi.SetReply, error) {
 	key := in.GetKey()
 	value := in.GetValue()
 	log.Printf("serving set request for key %q and value %q", key, value)
 
 	s.data[key] = value
 
-	reply := &pb.SetReply{}
+	reply := &kvapi.SetReply{}
 	return reply, nil
 }
 
 // Get returns a value associated with a key to the client
-func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetReply, error) {
+func (s *server) Get(ctx context.Context, in *kvapi.GetRequest) (*kvapi.GetReply, error) {
 	key := in.GetKey()
 	log.Printf("serving get request for key %q", key)
 
@@ -45,7 +45,7 @@ func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetReply, erro
 		return nil, errKeyNotFound
 	}
 
-	reply := &pb.GetReply{
+	reply := &kvapi.GetReply{
 		Value: value,
 	}
 	return reply, nil
@@ -61,7 +61,7 @@ func main() {
 		data: make(map[string]string),
 	}
 	s := grpc.NewServer()
-	pb.RegisterKeyValueServer(s, &serverInstance)
+	kvapi.RegisterKeyValueServer(s, &serverInstance)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
